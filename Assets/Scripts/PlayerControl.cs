6d4ccac95a81c7d675bdbe014afa9757e6d55f3d
Gabriel Russell <18153765+Kaisogen@users.noreply.github.com>
2017-10-29 14:33:02 -0400

    Animation work
    Unstable changes, just trying to add animations.

--- Assets/Scripts/PlayerControl.cs
+++ Assets/Scripts/PlayerControl.cs
@@ -10,11 +10,37 @@ public class PlayerControl : MonoBehaviour
 
 	private Vector2 adjustment;
 	private Rigidbody2D rigidbodyComponent;
+	SpriteRenderer sprRenderer;
+	Animator anim;
+
+	bool WalkIsTrue;
+
+	void Start() {
+		sprRenderer = GetComponent<SpriteRenderer> ();
+		anim = gameObject.GetComponent<Animator> ();
+		WalkIsTrue = false;
+	}
 
 	void Update() {
 
 		float horizontalInput = Input.GetAxis("Horizontal");
 		float verticalInput = Input.GetAxis("Vertical");
+
+		if (Input.GetKeyDown ("a")) {
+			sprRenderer.flipX = false;
+			WalkIsTrue = true;
+		} else if (Input.GetKeyDown ("d")) {
+			sprRenderer.flipX = true;
+			WalkIsTrue = true;
+		} else {
+			WalkIsTrue = false;
+		}
+
+		if (WalkIsTrue == false) {
+			anim.SetBool ("WalkIsTrue", false);
+		} else if (WalkIsTrue == true) {
+			anim.SetBool ("WalkIsTrue", true);
+		}
 
 		adjustment = new Vector2(speed.x * horizontalInput, speed.y * verticalInput);
 	}
