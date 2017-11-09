7e48519f0997c36f55f785fb4f8d424218ecdf8a
TeamHud <hud362217115@gmail.com>
2017-11-08 23:10:27 -0500

    Add files via upload
    So this dude now fires volleys of toast instead of bullets. I like dis.

---
+++ PlayerControl.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+//Player controls for on the field.
+public class PlayerControl : MonoBehaviour
+{
+	public Vector2 speed = new Vector2(50, 50);
+
+	private Vector2 adjustment;
+	private Rigidbody2D rigidbodyComponent;
+	SpriteRenderer sprRenderer;
+	void Start() {
+		print(speed);
+	
+		sprRenderer = GetComponent<SpriteRenderer> ();
+	}
+
+	void Update() {
+
+		float horizontalInput = Input.GetAxis ("Horizontal");
+		float verticalInput = Input.GetAxis ("Vertical");
+	
+		adjustment = new Vector2 (speed.x * horizontalInput, speed.y * verticalInput);
+
+	}
+
+	void FixedUpdate() {
+		if (rigidbodyComponent == null) rigidbodyComponent = GetComponent<Rigidbody2D>();
+
+		rigidbodyComponent.velocity = adjustment;
+	}
+}
