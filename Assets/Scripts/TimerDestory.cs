dfd3a0e749997e2b5d0da4ca4593b8387ff49bfa
Robert <hud362217115@gmail.com>
2017-11-11 21:32:36 -0500

    Bullets now have shorter life span due to budget cuts.

---
+++ Assets/Scripts/TimerDestory.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TimerDestory : MonoBehaviour {
+		public float lifetime;
+
+		void Start ()
+		{
+			Destroy (gameObject, lifetime);
+		}
+	}
