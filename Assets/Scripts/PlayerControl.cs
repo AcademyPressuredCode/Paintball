912a1301620d18b57734878141c45243e7c0b95f
Gabriel Russell <TheBlargbla@gmail.com>
2018-01-24 21:38:10 -0500

    Add UI tests
    UI is completely untested, so I have zero idea if it works. It loads in the game window in the IDE, so I guess so.

--- Assets/Scripts/PlayerControl.cs
+++ Assets/Scripts/PlayerControl.cs
@@ -5,28 +5,22 @@ using UnityEngine;
 //Player controls for on the field.
 public class PlayerControl : MonoBehaviour
 {
-	public Vector2 speed = new Vector2(50, 50);
-	Vector2 PlayerPos;
+    public GameObject GUI;
+    public Vector2 speed = new Vector2(50, 50);
 	private Vector2 adjustment;
 	private Rigidbody2D rigidbodyComponent;
-
+    
 
 	void Update() {
-        //Find Mouse Position
-        Vector2 mousePos = Input.mousePosition;
-
-        //Find the angle from the player position in Radians
-        float angle = Vector2.Angle(PlayerPos, mousePos);
-
-		//Adjust to Degrees
-		float deg = angle * Mathf.Rad2Deg;
-        Debug.Log("Angle??: " + angle);
-
 		float horizontalInput = Input.GetAxis ("Horizontal");
 		float verticalInput = Input.GetAxis ("Vertical");
 	
 		adjustment = new Vector2 (speed.x * horizontalInput, speed.y * verticalInput);
 
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            GUI.SetActive(true);
+        }
 	}
 
 	void FixedUpdate() {
