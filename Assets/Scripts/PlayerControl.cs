8e009901eae8af44a3c102e589555f9556c07118
Gabriel Russell <TheBlargbla@gmail.com>
2018-01-24 21:48:34 -0500

    More UI changes
    Can re-enable / disable it

--- Assets/Scripts/PlayerControl.cs
+++ Assets/Scripts/PlayerControl.cs
@@ -9,6 +9,7 @@ public class PlayerControl : MonoBehaviour
     public Vector2 speed = new Vector2(50, 50);
 	private Vector2 adjustment;
 	private Rigidbody2D rigidbodyComponent;
+    private bool GUIon;
     
 
 	void Update() {
@@ -17,9 +18,14 @@ public class PlayerControl : MonoBehaviour
 	
 		adjustment = new Vector2 (speed.x * horizontalInput, speed.y * verticalInput);
 
-        if (Input.GetKeyDown(KeyCode.Escape))
+        if (Input.GetKeyUp(KeyCode.Escape) && GUIon == false)
         {
             GUI.SetActive(true);
+            GUIon = true;
+        } else if (Input.GetKeyUp(KeyCode.Escape) && GUIon == true)
+        {
+            GUI.SetActive(false);
+            GUIon = false;
         }
 	}
 
