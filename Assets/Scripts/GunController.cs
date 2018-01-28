eaec1aef64b015ff47f1883144f6be07758956f8
Gabriel Russell <TheBlargbla@gmail.com>
2018-01-28 15:33:24 -0500

    Varmint Rifle
    Made by Costas, this tiny rifle packs a punch

--- Assets/Scripts/GunController.cs
+++ Assets/Scripts/GunController.cs
@@ -9,12 +9,15 @@ public class GunController : MonoBehaviour {
 	public GameObject Gun;
 	public float restTimer = 0;
     public Image cooldown;
+    public Animator anim;
 
 	void Update () {
         //Is the mouse button down and rest set to zero?
 		if (Input.GetMouseButtonDown (0) && restTimer <= 0) { //Spawn your bullet
 			GameObject bulletSpawn = Instantiate (toastBullet, Gun.transform.position, Quaternion.identity);
 			bulletSpawn.GetComponent<BulletScript>().Gun = gameObject;
+            anim.SetBool("Firing", true);
+
 
             //Decide how long the bullet should rest
             if (PlayerPrefs.GetString("Weapon") == "Pistol")
@@ -24,8 +27,11 @@ public class GunController : MonoBehaviour {
             {
                 restTimer = 120;
             }
-
-		}
+            
+		} else
+        {
+            anim.SetBool("Firing", false);
+        }
 
         //Give the cooldown meter it's length
         cooldown.fillAmount = restTimer / 100;
@@ -38,7 +44,11 @@ public class GunController : MonoBehaviour {
         //Only subtract the restTimer if it's greater than zero.
         if (restTimer > 0)
         {
+            anim.SetBool("Jammed", true);
             restTimer = restTimer - 1;
+        } else
+        {
+            anim.SetBool("Jammed", false);
         }
     }
 }
